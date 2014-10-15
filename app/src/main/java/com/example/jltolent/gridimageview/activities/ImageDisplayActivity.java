ead0cb178173d5d588d9fa3fdaea9dfa910eeba3
Jay Tolentino <jay@nuzzel.com>
2014-10-14 19:10:04 -0700

    working on share functionality

--- app/src/main/java/com/example/jltolent/gridimageview/activities/ImageDisplayActivity.java
+++ app/src/main/java/com/example/jltolent/gridimageview/activities/ImageDisplayActivity.java
@@ -2,27 +2,37 @@ package com.example.jltolent.gridimageview.activities;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
-import android.graphics.Point;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
-import android.media.Image;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.view.MenuItemCompat;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
+import android.support.v7.widget.ShareActionProvider;
 
 import com.example.jltolent.gridimageview.R;
 import com.example.jltolent.gridimageview.models.ImageResult;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 public class ImageDisplayActivity extends Activity {
+    private ShareActionProvider miShareAction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_display);
 
-        getActionBar().hide();
+        final ProgressDialog progress = new ProgressDialog(this);
+        progress.setTitle("Loading...");
+        progress.setMessage("Please wait.");
+        progress.show();
+
         ImageResult result = (ImageResult) getIntent().getSerializableExtra("result");
         ImageView ivImageResult = (ImageView) findViewById(R.id.ivImageResult);
         Picasso.with(this)
@@ -30,13 +40,26 @@ public class ImageDisplayActivity extends Activity {
                 .placeholder(R.drawable.ic_launcher)
                 .resize(600, 600)
                 .centerInside()
-                .into(ivImageResult);
+                .into(ivImageResult, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        setupShareIntent();
+                        progress.dismiss();
+                    }
+
+                    @Override
+                    public void onError() {
+
+                    }
+                });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.image_display, menu);
+        MenuItem item = menu.findItem(R.id.action_share);
+        miShareAction = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
         return true;
     }
 
@@ -50,5 +73,26 @@ public class ImageDisplayActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setupShareIntent() {
+        ImageView ivImageResult = (ImageView) findViewById(R.id.ivImageResult);
+        Uri bmpUri = getLocalBitmapUri(ivImageResult);
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+        shareIntent.setType("image/*");
+        miShareAction.setShareIntent(shareIntent);
+    }
+
+    private Uri getLocalBitmapUri(ImageView imageView) {
+        Drawable mDrawable = imageView.getDrawable();
+        Bitmap mBitmap = ((BitmapDrawable)mDrawable).getBitmap();
+
+        String path = MediaStore.Images.Media.insertImage(getContentResolver(),
+                mBitmap, "Description", null);
+
+        Uri uri = Uri.parse(path);
+        return uri;
     }
 }
